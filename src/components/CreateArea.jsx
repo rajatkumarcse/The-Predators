a2c27c0098ce314f541971e8119ee1668ce28c03
rajatkumar <rajat1273singh@gmail.com>
2023-10-15 14:34:29 +0530

    stuff added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -4,7 +4,7 @@ const CreateArea = () => {
   return (
     <div className="rewards-pane">
       <h1 className="rewards-title">Rewards for paying credit card bills</h1>
-      <p className="rewards-description">Earn rewards for paying your credit card bills on time and in full. You can redeem your rewards for cash back, travel, merchandise, and more.</p>
+      <p className="rewards-description">Earn new rewards for paying your credit card bills on time and in full. You can redeem your rewards for cash back, travel, merchandise, and more.</p>
     </div>
   );
 };
