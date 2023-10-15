40f64bf28705210527dd2ae6c786f26788116131
Sharang Kalra <sharangkalra01@gmail.com>
2023-10-15 14:22:20 +0530

    Nav-FrontEnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Header from "./components/Header"; 
+import "./styles/header.scss"; 
+import "./styles/App.scss"; 
 function App() {
   return (
     <Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"; 
+import logo from "../Assets/Logo2.jpeg"
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>   
+        <div>
+            <h1>dataInsightsX</h1>
+            <img src={logo} alt="dataInsightsX-Logo" />
+        </div>
+        <main>
+            <Link to={"/"}>Home</Link>
+            <Link to={"#about"}>About</Link>
+        </main>
+        
+    </nav>
   )
 }
 
